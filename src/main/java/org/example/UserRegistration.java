dadc450391cdb93bd81c6986e9eced3182a6c78e
Srinath S <srinath@Srinaths-MacBook-Air.local>
2021-12-29 10:35:25 +0530

    Successfully Added

--- src/main/java/org/example/UserRegistration.java
+++ src/main/java/org/example/UserRegistration.java
@@ -66,7 +66,7 @@ import java.util.regex.Pattern;
         if(isEmail)
             System.out.println(email+" is a Valid Email address\n");
         else
-            System.out.println(email+" is an Invalid Email address");
+            System.out.println(email+" is an Invalid Email address\n");
     }
 
     /**
@@ -138,6 +138,86 @@ import java.util.regex.Pattern;
         checkValidPassword(password);
 
         scannerObject.close();
+
+
+        /**
+         UC-9 Should clear all email samples provided separately
+         */
+
+        System.out.println("---------- Valid Emails ----------");
+
+        email = "abc@yahoo.com";
+        checkValidEmail(email);
+
+        email = "abc-100@yahoo.com";
+        checkValidEmail(email);
+
+        email = "abc.100@yahoo.com";
+        checkValidEmail(email);
+
+        email = "abc111@abc.com";
+        checkValidEmail(email);
+
+        email = "abc-100@abc.net";
+        checkValidEmail(email);
+
+        email = "abc.100@abc.com.au";
+        checkValidEmail(email);
+
+        email = "abc@1.com";
+        checkValidEmail(email);
+
+        email = "abc@gmail.com.com";
+        checkValidEmail(email);
+
+        email = "abc+100@gmail.com";
+        checkValidEmail(email);
+
+        System.out.println("---------------------------------------------------");
+
+        System.out.println("---------- Invalid Emails ----------");
+
+
+        email = "abc";
+        checkValidEmail(email);
+
+        email = "abc@.com.my";
+        checkValidEmail(email);
+
+        email = "abc123@gmail.a";
+        checkValidEmail(email);
+
+        email = "abc123@.com";
+        checkValidEmail(email);
+
+        email = "abc123@.com.com";
+        checkValidEmail(email);
+
+        email = ".abc@abc.com";
+        checkValidEmail(email);
+
+        email = "abc()*@gmail.com";
+        checkValidEmail(email);
+
+        email = "abc@%*.com";
+        checkValidEmail(email);
+
+        email = "abc..2002@gmail.com";
+        checkValidEmail(email);
+
+        email = "abc.@gmail.com";
+        checkValidEmail(email);
+
+        email = "abc@abc@gmail.com";
+        checkValidEmail(email);
+
+        email = "abc@gmail.com.1a";
+        checkValidEmail(email);
+
+        email = "abc@gmail.com.aa.au";
+        checkValidEmail(email);
+
+        System.out.println("---------------------------------------------------");
 
     }
 }
