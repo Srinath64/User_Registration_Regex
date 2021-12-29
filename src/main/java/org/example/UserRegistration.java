c3443b161a40753fdb1fc7a9d3d85096e9af95e7
Srinath S <srinath@Srinaths-MacBook-Air.local>
2021-12-29 09:28:38 +0530

    Successfully Added

--- src/main/java/org/example/UserRegistration.java
+++ src/main/java/org/example/UserRegistration.java
@@ -48,6 +48,27 @@ import java.util.regex.Pattern;
             System.err.println(lastName + " is an Invalid Last Name");
     }
 
+    /**
+     UC-3 User need to enter a valid Email
+     */
+
+    public static void checkValidEmail(String email){
+
+        boolean isEmail;
+        String emailRegex = "^[a-zA-Z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$";
+        Pattern patternObject = Pattern.compile(emailRegex);
+        if (email == null) {
+            isEmail = false;
+        }
+        Matcher matcherObject = patternObject.matcher(email);
+        isEmail =  matcherObject.matches();
+
+        if(isEmail)
+            System.out.println(email+" is a Valid Email address\n");
+        else
+            System.out.println(email+" is an Invalid Email address");
+    }
+
 
     public static void main(String[] args) {
 
@@ -62,6 +83,10 @@ import java.util.regex.Pattern;
         System.out.println("Enter Your Last Name");
         String lastName = scannerObject.next();
         checkValidLastName(lastName);
+
+        System.out.println("Enter Your Email Address");
+        String email = scannerObject.next();
+        checkValidEmail(email);
 
         scannerObject.close();
 
